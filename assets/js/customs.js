e0107c387695eb66c7b886990a59d11b6a9f8ea7
quyentt <quyentt1985@gmail.com>
2024-03-01 12:19:21 +0700

    first commit

---
+++ assets/js/customs.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $(".menu-more").click(function () {
+    $(".menu-more-content").addClass("show");
+  });
+  $(".menu-more-close").click(function () {
+    $(".menu-more-content").removeClass("show");
+  });
+  // ----------match detail
+  var swiper_match_nav = new Swiper(".match-nav-slider", {
+    spaceBetween: 0,
+    slidesPerView: "auto",
+    freeMode: false,
+    watchSlidesProgress: false,
+  });
+  var swiper_match_content = new Swiper(".match-content-slider", {
+    autoHeight: true,
+    spaceBetween: 0,
+    navigation: false,
+    thumbs: {
+      swiper: swiper_match_nav,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+  });
+});
